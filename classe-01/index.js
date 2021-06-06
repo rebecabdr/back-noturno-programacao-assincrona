84fe28e08c6928996d11d90ff5a34448240893d8
Rebeca Rocha <rebecabdrocha@gmail.com>
2021-06-06 14:47:38 -0300

    letra A resolvida no insomnia

--- classe-01/index.js
+++ classe-01/index.js
@@ -1,41 +1,56 @@
 const axios = require('axios');
+const express = require('express');
+
+const app = express();
 
 
 
 // letra A - sem id e sem nome
+
+app.get("/pokemon/", (req, res) => {
+    
+    const inicio = Number(req.query.offset);
+    const quantidade = Number(req.query.limit);
+    if (!req.query.offset || !req.query.limit){
+        res.json("É necessário inserir um valor inicial e a quantidade, favor revisar!")
+    } else {
+        getPokeList(inicio, quantidade).then((x) => res.json(x));
+    }
+});
 
 async function getPokeList (offset, limit){
     const response = await axios.get(` https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`);
     return response.data.results
 };
 
-getPokeList(0, 3).then((x) => console.log(x));
+
 
 // letra B
-async function getPokeIdName (idName){
-    const promiseOrigin = await axios.get(` https://pokeapi.co/api/v2/pokemon/${idName}`);
-    const {
-        id,
-        name,
-        height,
-        weight,
-        base_experience,
-        forms,
-        abilities,
-        species
-    } = promiseOrigin.data;
+// async function getPokeIdName (idName){
+//     const promiseOrigin = await axios.get(` https://pokeapi.co/api/v2/pokemon/${idName}`);
+//     const {
+//         id,
+//         name,
+//         height,
+//         weight,
+//         base_experience,
+//         forms,
+//         abilities,
+//         species
+//     } = promiseOrigin.data;
 
-    return {
-        id,
-        name,
-        height,
-        weight,
-        base_experience,
-        forms,
-        abilities,
-        species
-    }
-};
+//     return {
+//         id,
+//         name,
+//         height,
+//         weight,
+//         base_experience,
+//         forms,
+//         abilities,
+//         species
+//     }
+// };
 
-getPokeIdName(1).then((x) => console.log(x));
+// getPokeIdName(1).then((x) => console.log(x));
 
+app.listen(8000);
